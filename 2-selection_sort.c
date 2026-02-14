ed51839718e8a7042a49b23a08ecaf419e5a372e
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-02-14 22:56:29 +0400

    Update, Task 2, Betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, minimum_index;
-    int temp;
+	size_t i, j, minimum_index;
+	int temp;
 
-    if (array == NULL || size < 2)
-    return;
+	if (array == NULL || size < 2)
+	return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        minimum_index = i;
-        for(j = i + 1; j < size; j++)
-        {
-            if(array[j] < array[minimum_index])
-                minimum_index = j;
-        }
-        if (minimum_index != i)
-        {
-            temp = array[i];
-            array[i] = array[minimum_index];
-            array[minimum_index] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		minimum_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minimum_index])
+				minimum_index = j;
+		}
+		if (minimum_index != i)
+		{
+			temp = array[i];
+			array[i] = array[minimum_index];
+			array[minimum_index] = temp;
+			print_array(array, size);
+		}
+	}
 }
