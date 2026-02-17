63d97f41cf5c12125e8f3d343046c3ede0ac4b65
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-02-17 22:32:39 +0400

    Update, Task 3. Fix the over-printing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -34,12 +34,10 @@ int partition(int *array, int low, int high, size_t size)
 
 	}
 
-	if (i + 1 != high)
-	{
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	print_array(array, size);
 
 	return (i + 1);
 }
