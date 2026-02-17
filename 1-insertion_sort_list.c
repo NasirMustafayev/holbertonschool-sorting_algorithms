e891a79fca18ec84371fd29173b6413318c061fb
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-02-17 23:14:40 +0400

    Task 1. Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: A pointer to the head of the list
+ * Return: Nothing
+ * Description: Prints the list after each swap of two nodes
+ */
+
+void insertion_sort_list(listint_t **list)
+{    listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			/* Swap temp and temp->prev */
+			if (temp->prev->prev != NULL)
+				temp->prev->prev->next = temp;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			/* Update head of the list if necessary */
+			if (temp->prev == NULL)
+				*list = temp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
